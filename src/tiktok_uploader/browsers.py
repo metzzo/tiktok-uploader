3b70822eb97050747932d39a67db5c5ade6ff574
Robert Fischer <robert@programming-with-design.at>
2024-05-28 13:47:06 +0200

    make docker ready

--- src/tiktok_uploader/browsers.py
+++ src/tiktok_uploader/browsers.py
@@ -84,9 +84,12 @@ def chrome_defaults(*args, headless: bool = False, proxy: dict = None, **kwargs)
     options = ChromeOptions()
 
     ## regular
+    options.add_argument('--no-sandbox')
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument('--profile-directory=Default')
     options.add_argument("--window-size=1920,1080")
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
 
     ## experimental
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
